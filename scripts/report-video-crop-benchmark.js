5c7d868b46bf45c54850e0e8478918849eb45e20
Mahendra Kumawat <mahendrakumawat80224@gmail.com>
2026-07-07 02:21:46 +0530

    Cleanup Chinese text in scripts/report-video-crop-benchmark.js

--- scripts/report-video-crop-benchmark.js
+++ scripts/report-video-crop-benchmark.js
@@ -195,7 +195,7 @@ function parseCliArgs(argv) {
         } else if (arg === '--help' || arg === '-h') {
             parsed.help = true;
         } else {
-            throw new Error(`未知参数: ${arg}`);
+            throw new Error(`Unknown argument: ${arg}`);
         }
     }
 
